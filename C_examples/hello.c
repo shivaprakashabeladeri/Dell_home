cc4a4987fb02fee1d82910ca93beffe90fff5ca5
Shivaprakasha Beladeri <beladerishiva@outlook.com>
2015-12-22 19:13:10 -0800

    Changed the comment section

--- C_examples/hello.c
+++ C_examples/hello.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-// This is the entry function
+/* This is the entry function */
 int main()
 {
-	// This will print the Hello World message on the standard output
+	/* This will print the "Hello World" message on the standard output */
 	printf("Hello World\n");
 	return 0;
 }
